5d4864ddbc5988da467e3295d74e8260f90129fa
Chae-G-0 <corkdud@naver.com>
2022-12-30 18:03:57 +0900

    221230

--- dist/function.js
+++ dist/function.js
@@ -10,3 +10,24 @@ function hello(name) {
 }
 var result = hello();
 var result2 = hello("sam");
+// 선택적 매개변수가 필수 매개변수보다 뒤에 있어야 한다. 앞에 있으면 에러가 발생한다.옵셔널한 값은 입력을 안해도 된다느 의미이기 때문에 성립할 수 없다.
+function hi(name, age) {
+    if (age !== undefined) {
+        return "hello, ".concat(name, ", you are ").concat(age);
+    }
+    else {
+        return "hello, ".concat(name);
+    }
+}
+console.log(hi("Sam"));
+console.log(hi("sam", 30));
+// 나머지 매개변수 타입 작성법
+function plus() {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    return nums.reduce(function (result, num) { return result + num; }, 0);
+}
+plus(1, 2, 3);
+console.log(plus(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
--- src/function.ts
+++ src/function.ts
@@ -13,3 +13,27 @@ function hello(name?:string) {
 
 const result = hello();
 const result2 = hello("sam");
+
+
+// 선택적 매개변수가 필수 매개변수보다 뒤에 있어야 한다. 앞에 있으면 에러가 발생한다.옵셔널한 값은 입력을 안해도 된다느 의미이기 때문에 성립할 수 없다.
+function hi(name: string, age?: number): string {
+    if (age !== undefined) {
+        return `hello, ${name}, you are ${age}`;
+    } else {
+        return `hello, ${name}`;
+    }
+}
+
+console.log(hi("Sam"));
+console.log(hi("sam", 30))
+
+
+// 나머지 매개변수 타입 작성법
+
+function plus(...nums:number[]) {
+    return nums.reduce((result, num)=>result + num, 0)
+}
+
+plus(1, 2, 3);
+console.log(plus(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
